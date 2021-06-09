2188d3fd6e3e8074cf5865f025f8ee416f7eb695
IIPuBeT-AHgpeu <65333308+IIPuBeT-AHgpeu@users.noreply.github.com>
2021-06-09 13:22:46 +0300

    Add files via upload

---
+++ CodeAndroid/Model.java
@@ -0,0 +1,20 @@
+package com.example.test_sqlite;
+
+public class Model {
+    public static class User
+    {
+        public int Id;
+        public String Login;
+        public String Password;
+    }
+    public static class Way
+    {
+        public int Number;
+        public int Use_count;
+    }
+    public static class Text
+    {
+        public int Id;
+        public String Text;
+    }
+}
