5882709d3619e84ad074157418d39b668ab1f9ed
IIPuBeT-AHgpeu <65333308+IIPuBeT-AHgpeu@users.noreply.github.com>
2021-12-13 15:39:12 +0300

    Delete dbms_5 directory
    Аналогично 4 лабе по субд.

--- dbms_5/config/db.py
+++
@@ -1,2 +0,0 @@
-from pymongo import MongoClient
-conn = MongoClient("mongodb+srv://cyber_essence:Tbz9uv5m6fS2HEc@cluster0.ha8y8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
--- dbms_5/models/user.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class User(BaseModel):
-    cat_id: int
-    login: str
-    password: str
-    name: str   
